735a4bd4433edf9194afa5ec6398e1029b974699
Rama Komarudin Soemardja <rsoemardja@gmail.com>
2024-02-06 17:31:13 +1100

    Finished Day 6

---
+++ Day-6/main.py
@@ -0,0 +1,12 @@
+print("SECURE LOGIN")
+username = input("What is your username?")
+password = input("What is your password?")
+
+if username == "Rama" and password == "PyTh0n":
+  print("Welcome Rama! You are looking nice today!")
+elif username == "Jimmy" and password == "J1mmy":
+  print("Hey there Jimmy! Nice work today")
+elif username == "Jack" and password == "J4ck":
+  print("Yo! Jack! What up?!")
+else:
+  print("Go away!")
